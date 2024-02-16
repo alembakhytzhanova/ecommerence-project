685235e0c4beeb198a9983bcd544d6771af217f6
mirvision <florangorr@gmail.com>
2024-02-16 23:14:11 +0600

    started and added routes with main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"alem/routes"
+	"controllers"
+	"database"
+	"log"
+	"middleware"
+	"os"
+	"routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Product"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.addToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+
+}
