40b81f39a57afd4347c01343607b8a9c930db9e9
mirvision <florangorr@gmail.com>
2024-02-21 20:20:50 +0600

    ecommerence part 8

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"alem/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/singup", controllers.SignUp())
